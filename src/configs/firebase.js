f3216161d253a56d4cf236fa4391e91b31c180d3
CyberGabriel <gabrielciberneticu@gmail.com>
2021-11-08 15:23:46 +0200

    firebase

---
+++ src/configs/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDl3R5IwypfKEq35ri5zvL8FzwjcvwsB1w",
+    authDomain: "fir-gg-b374b.firebaseapp.com",
+    projectId: "fir-gg-b374b",
+    storageBucket: "fir-gg-b374b.appspot.com",
+    messagingSenderId: "171254373772",
+    appId: "1:171254373772:web:ddb3c7abc453c5d97d8aa9"
+  };
+  
+export default firebaseConfig;
